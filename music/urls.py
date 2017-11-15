57a4644a41ffbbaae5666634801f19808771e313
Miguel Pinto <miguelpinto25@hotmail.com>
2017-11-15 09:28:08 +0000

    Added view paths in the config files

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'), # Setup home page for each individual app ^ is begin and $ is end
+    # Setup home page for each individual app ^ is begin and $ is end
+    url(r'^$', views.index, name='index'),
+
+    url(r'^(?P<album_id>[0-9]+)$', views.details, name="details"),
 ]
