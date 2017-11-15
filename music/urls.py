4b0bfb42c88611400521deb7c84152c0cdf89dec
Miguel Pinto <miguelpinto25@hotmail.com>
2017-11-15 09:28:44 +0000

    Added slash at the end for the path to be correct

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # Setup home page for each individual app ^ is begin and $ is end
     url(r'^$', views.index, name='index'),
 
-    url(r'^(?P<album_id>[0-9]+)$', views.details, name="details"),
+    url(r'^(?P<album_id>[0-9]+)/$', views.details, name="details"),
 ]
