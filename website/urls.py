262a7f8905ffe53b850375c37040f27b06d911b4
Miguel Pinto <miguelpinto25@hotmail.com>
2017-11-21 10:21:44 +0000

    Updated urls from website

--- website/urls.py
+++ website/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^music/', include('music.urls')),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^tasks/', include('tasks.urls')),
 ]
